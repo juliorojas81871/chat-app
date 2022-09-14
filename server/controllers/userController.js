71f53db740d6a8568e354748dfb4de882ad77c7a
Julio <juliorojas81871@gmail.com>
2022-09-13 23:23:06 -0400

    finish login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,12 +5,14 @@ module.exports.register = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const usernameCheck = await User.findOne({ username });
-    if (usernameCheck) {
-      return res.json({ msg: "Username already used", status: false });
-    }
     const emailCheck = await User.findOne({ email });
-    if (emailCheck) {
-      return await User.findOne({ msg: "Email already used", status: false });
+
+    if (username && emailCheck) {
+      return res.json({ msg: "Username & Email already used", status: false });
+    } else if (usernameCheck) {
+      return res.json({ msg: "Username already used", status: false });
+    } else if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
@@ -18,6 +20,24 @@ module.exports.register = async (req, res, next) => {
       username,
       password: hashedPassword,
     });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    }
     delete user.password;
     return res.json({ status: true, user });
   } catch (ex) {
